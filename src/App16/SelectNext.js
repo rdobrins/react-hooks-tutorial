602816a69173fc575c14ebc36bf90d89191480da
rdobrins <rdobrinski@linksquares.com>
2022-03-11 11:36:28 -0500

    confetti stuff

--- src/App16/SelectNext.js
+++ src/App16/SelectNext.js
@@ -1,14 +1,18 @@
 import React, { useContext, useState, Fragment } from 'react'
 import BookClubContext from './BookClubContext'
+import JSConfetti from 'js-confetti'
 
 const SelectNext = () => {
   const [nextHost, setNextHost] = useState(null)
+  const possiblesStates = ['first', 'second', 'third']
 
   const {
     pool,
     poolPresent,
     selectFromPool
   } = useContext(BookClubContext)
+
+  const jsConfetti = new JSConfetti()
 
   if (!poolPresent()) return null
 
@@ -23,9 +27,28 @@ const SelectNext = () => {
     setNextHost(selectFromPool())
   }
 
+  const fireConfetti = () => {
+    setInterval(() => { jsConfetti.addConfetti() }, 1000)
+  }
+
+  const handleHype = () => {
+    let n = 0
+
+    let ryansInterval = setInterval(() => {
+      if (possiblesStates[n]) {
+        setNextHost(possiblesStates[n])
+        n++
+      } else {
+        clearInterval(ryansInterval)
+        fireConfetti()
+        handleSelection()
+      }
+    }, 1000)
+  }
+
   return(
     <div style={selectNextWrapperStyling}>
-      <button onClick={handleSelection}>
+      <button onClick={handleHype}>
         Select Host for Next Week
       </button>
       { nextHost && <h1>{ nextHost }</h1> }
